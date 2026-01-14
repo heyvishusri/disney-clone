d3acb4067af4434f149927392f029a2537d328d5
heyvishusri <vishwaskumar55347@gmail.com>
2026-01-15 01:17:48 +0530

    Initial commit - disney clone setup

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../assets/Images/logo.png";
+
+const Header = () => {
+  return (
+    <div>
+      <img src={logo} className="w-[80px] md:w-[115px] object-cover" />
+    </div>
+  );
+};
+
+export default Header;
